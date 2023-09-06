ec7c456872a4ca6ff62db615f88b5553b5af7140
pedroduartedg2 <opedrohenriqueduarte@gmail.com>
2023-09-06 18:52:57 -0300

    ajustando

--- pages/Home/script.js
+++ pages/Home/script.js
@@ -3,6 +3,12 @@ import { logout, verifyLogin } from "../../firebase-init.js";
 verifyLogin();
 
 document.getElementById("logout-btn").addEventListener("click", () => {
-  console.log("clicou sair");
   logout();
 });
+
+document.getElementById("send-btn").addEventListener("click", () => {
+  let message = document.getElementById("input-message").value;
+  console.log(message);
+});
+
+const sendMessage = () => {};
