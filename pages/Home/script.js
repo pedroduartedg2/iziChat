68cfded1a828bbfd9ce0f3636321e5cdd3c4728d
pedroduartedg2 <opedrohenriqueduarte@gmail.com>
2023-09-08 14:08:54 -0300

    ajuste rolagem

--- pages/Home/script.js
+++ pages/Home/script.js
@@ -232,7 +232,7 @@ function verificaRolagem() {
   var distanciaRolada = window.scrollY || window.pageYOffset;
   var divFlutuante = document.getElementById("div-flutuante");
 
-  if (distanciaRolada < 110) {
+  if (distanciaRolada < 200) {
     // Ajuste o valor conforme necessário
     divFlutuante.style.display = "block";
   } else {
