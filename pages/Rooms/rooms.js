03df56474ef7e9d67fb6ca0be2de532d71996b06
pedroduartedg2 <opedrohenriqueduarte@gmail.com>
2023-09-10 23:23:37 -0300

    teste notification

--- pages/Rooms/rooms.js
+++ pages/Rooms/rooms.js
@@ -126,6 +126,29 @@ cancelBtn.addEventListener("click", () => {
 });
 
 // Notificações
+
+const button = document.getElementById("notifications");
+button.addEventListener("click", () => {
+  Notification.requestPermission().then((result) => {
+    if (result === "granted") {
+      randomNotification();
+    }
+  });
+});
+
+function randomNotification() {
+  const randomItem = Math.floor(Math.random());
+  const notifTitle = "tirulo";
+  const notifBody = `teste`;
+  const notifImg = "../src/logo.png";
+  const options = {
+    body: notifBody,
+    icon: notifImg,
+  };
+  new Notification(notifTitle, options);
+  setTimeout(randomNotification, 30000);
+}
+
 // Verifique se o navegador suporta notificações
 // if ("Notification" in window) {
 //   // Verifique se as notificações estão desativadas
